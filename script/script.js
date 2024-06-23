2dbfdade1ac96a38bf7b65ecb545a04b040fe1dd
Wing Catcher <catcherwing@gmail.com>
2024-06-23 22:11:09 +0700

    modified:   css/style.css 	modified:   index.html 	new file:   script/script.js

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function alerted(){
+    let abe = document.getElementById("text").value
+    if(abe == ""){
+        alert("none")
+    }
+    else if(abe == "hello"){
+        alert("Hiiii")
+    }
+    else{
+        alert(abe)
+    }
+}
