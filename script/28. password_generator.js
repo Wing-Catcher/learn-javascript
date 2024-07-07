54d1de58641110a0d8e7f17f4c8f15ec14120672
Wing Catcher <catcherwing@gmail.com>
2024-07-07 21:16:03 +0700

    new file:   css/28. password_generator.css 	new file:   html/28. password_generator.html 	modified:   index.html 	new file:   script/28. password_generator.js

---
+++ script/28. password_generator.js
@@ -0,0 +1,42 @@
+// Random Password Generator
+
+function generatePassword(length, includeLowerCase, includeUpperCase, includeSymbols, includeNumbers){
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "1234567890";
+    const symbolChars = "!@#$%^&*_+";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowerCase ? lowercaseChars : "";
+    allowedChars += includeUpperCase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if (length <= 0) {
+        return "(Password length must be at least 1.)";
+    }
+    if(allowedChars.length === 0){
+        return "(At least 1 set of characters needs to be selected)";
+    }
+    for (let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    return password
+}
+
+const passwordLength = 11;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, 
+                                  includeLowerCase, 
+                                  includeUpperCase, 
+                                  includeSymbols, 
+                                  includeNumbers);
+// fr bro i spent 1 hour for this "password length" shit
+console.log(`Generated Password ${password}`)
